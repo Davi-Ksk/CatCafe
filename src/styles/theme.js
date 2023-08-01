f95a671d654f506aca8ff359ae35222680049920
davi <davikraieski@gmail.com>
2023-08-01 05:03:46 -0300

    inital commit

---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+  // Quanto maior o número, mais clara é a cor
+  // The bigger the number, the lighter the color
+  COLORS: {
+
+    BACKGROUND_100: "#FBF8F1",
+    BACKGROUND_200: "#F7ECDE",
+    BACKGROUND_300: "#E9DAC1",
+
+    BOLD: "3F2305",
+    ACCENT: "#54BAB9",
+    
+    WHITE: "#FFFFFF",
+    BLACK: "#1E1E1E"
+  }
+}
